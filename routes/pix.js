00bdbe5e0c2c7535ea0c8f762d2828a6b18ac3d2
Mateus Roveda <mateus.roveda@universo.univates.br>
2023-11-30 22:06:01 -0300

    Ajustes pós bugs

--- routes/pix.js
+++ routes/pix.js
@@ -4,9 +4,10 @@ const UserModel = require('../models/User');
 const pixController = require('../controllers/PixController');
 
 const validateUserId = async (req, res, next) => {
-    if (req.params.userId === 'null') {
+    if (req.params.userId === 'null' || isNaN(req.params.userId)) {
         return res.status(404).json({ error: 'User not found' });
     }
+
     const user = await UserModel.findByPk(req.params.userId);
     if (!user) {
         return res.status(404).json({ error: 'User not found' });
@@ -14,7 +15,14 @@ const validateUserId = async (req, res, next) => {
     next();
 }
 
-router.post('/pix', pixController.create);
+const validateDifferentIds = async (req, res, next) => {
+    if (req.body.senderId === req.body.recipientId) {
+        return res.status(400).json({ error: 'hehehe, this is not allowed, brow!' });
+    }
+    next();
+}
+
+router.post('/pix', validateDifferentIds, pixController.create);
 
 router.get('/pix', pixController.index);
 
